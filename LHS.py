ca0f6e6357e41e873673b15a3c5158d21d6e3174
熊路远 <xiongluyuan@xiongluyuandeMacBook-Air.local>
2017-12-08 21:19:11 +0800

    add new function

---
+++ LHS.py
@@ -0,0 +1,30 @@
+import sys
+
+def list_traverse(L):
+    L1 = []
+    for i in range(len(L)):
+        length = 0
+        for j in range(i+1,len(L)):
+            if abs(L[j] - L[i]) <= 1:
+                length += 1
+        L1.append(length + 1);
+        
+        
+    return max(L1)
+
+def max_product(L):
+    max = 0
+    for i in range(len(L)-2):
+        product = L[i]
+        for j in range(i+1,i+3):
+            product *= L[j]
+        if product > max:
+            max = product
+    return max
+            
+            
+
+L = [1,3,2,2,5,2,3,7]
+print(list_traverse(L))
+
+print(max_product(L))
